e540cb54d84217a85b879cbf4a69420d0376460e
Grzegorz Sobocinski <grzegorz.sobocinski2397@gmail.com>
2020-03-09 18:30:54 +0100

    Configure webpack

---
+++ starter/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: './src/index.js',
+        path: path.resolve(__dirname, 'js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+              },
+        ]
+    }
+}
